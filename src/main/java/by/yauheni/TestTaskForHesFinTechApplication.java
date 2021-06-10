e6de6f7059cf61edb3181baab6e11e1caf5c299a
Женя <noximt@gmail.com>
2021-06-10 08:10:51 +0300

    Initial commit

---
+++ src/main/java/by/yauheni/TestTaskForHesFinTechApplication.java
@@ -0,0 +1,13 @@
+package by.yauheni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForHesFinTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskForHesFinTechApplication.class, args);
+    }
+
+}
